8c54addb4380b29f5ea912c8c9ae44a357a214fb
Sokrat <opiiumis@gmail.com>
2018-03-07 20:48:03 +0200

    17commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp       =require('gulp'),
 	sass       =require('gulp-sass'),
-	browserSync=require('browser-sync');
+	browserSync=require('browser-sync'),
+
+	del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
+    imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+    pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+    cache        = require('gulp-cache'); // Подключаем библиотеку кеширования
 	/*concat     =require('gulp-concat'),
 	uglify     =require('gulp-uglifyjs'),
 	cssnano    =require('gulp-cssnano'),
 	rename     =require('gulp-rename');*/
-
 const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass',function(){
@@ -50,3 +54,42 @@ gulp.task('default', () =>
         }))
         .pipe(gulp.dest('dist'))
 );
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*') // Берем все изображения из app
+        .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+
+gulp.task('build', ['clean', 'img', 'sass'], function() {
+
+    var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+        'app/css/main.css',
+        'app/css/libs.min.css'
+        ])
+    .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+    .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+    .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('clear', function () {
+    return cache.clearAll();
+})
+
+gulp.task('default', ['watch']);
